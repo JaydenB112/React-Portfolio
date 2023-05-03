b86de47513d484b90d48240bda16ea2763ee7556
Jayden Boyd <jaydenboydb1@gmail.com>
2023-05-03 17:08:26 -0500

    Ready to Rock

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Bootstrap} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
