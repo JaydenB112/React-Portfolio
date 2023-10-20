5f29a1ed2a79f32673c337f828c31bde2a88fde3
Jayden Boyd <jaydenboydb1@gmail.com>
2023-10-20 13:07:34 -0500

    Finished up updating page, now for projects!

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+import mongoDBIcon from "@iconify/icons-logos/mongodb-icon";
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import netlifyIcon from "@iconify/icons-logos/netlify-icon";
 
 class About extends Component {
   render() {
@@ -31,7 +31,7 @@ class About extends Component {
                     alt="Avatar placeholder"
                   />
                   <Icon
-                    icon={angularIcon}
+                    icon={mongoDBIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
@@ -39,7 +39,7 @@ class About extends Component {
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
-                    icon={vueIcon}
+                    icon={netlifyIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                 </span>
